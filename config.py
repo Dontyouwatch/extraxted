b3fcb27b4d01e64367781711461215e7098532f2
Dontyouwatch <97377754+Dontyouwatch@users.noreply.github.com>
2025-04-17 21:13:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7744338796:AAEmp0R9sUSIXfhHgJMwOnaYfNCPYgZUW24")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8080798222:AAFUfsajFp_vOeiF2eE-ituTbZ4HfIRKp20")
     API_ID = int(os.environ.get("API_ID", " 23454876"))
     API_HASH = os.environ.get("API_HASH", "b42626ee535fcaf915232af564a95bea")
     AUTH_USERS = "662494886"
