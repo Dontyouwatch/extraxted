95b86a8104b54b2e0285780c966f0296c0f572aa
Dontyouwatch <97377754+Dontyouwatch@users.noreply.github.com>
2025-04-17 21:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+e#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) ACE 
 
@@ -7,7 +7,7 @@ import os
 class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8080798222:AAFUfsajFp_vOeiF2eE-ituTbZ4HfIRKp20")
-    API_ID = int(os.environ.get("API_ID", " 23454876"))
+    API_ID = int(os.environ.get("API_ID", "23454876"))
     API_HASH = os.environ.get("API_HASH", "b42626ee535fcaf915232af564a95bea")
     AUTH_USERS = "662494886"
 
